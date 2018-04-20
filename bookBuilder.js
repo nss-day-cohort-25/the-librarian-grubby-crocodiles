dd7bb04f246cb45709677416eff3e8fb5e150caf
Riley Mathews <rileymathews80@gmail.com>
2018-04-20 11:11:18 -0500

    added basic database structure

---
+++ bookBuilder.js
@@ -0,0 +1,37 @@
+//function to push objects into the library array
+
+const bookBuidler = (title, author, genre, isbn) => {
+    Library.push(
+        Object.create({}, {
+            "title": {
+                value: title,
+                enumerable: true
+            },
+            "author": {
+                value: author,
+                enumerable: true
+            },
+            "genre": {
+                value: genre,
+                enumerable: true
+            },
+            "isbn": {
+                value: isbn,
+                enumerable: true
+            },
+            "checkedOut": {
+                value: false,
+                enumerable: true,
+                writable: true
+            },
+            "dueDate": {
+                value: "",
+                enumerable: true,
+                writable: true
+            }
+        })
+    )
+}
+
+
+//test calls 
