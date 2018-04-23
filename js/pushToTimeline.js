6240cd911db4e12a2d242ea7ff72a2dd8e2f947c
Riley Mathews <rileymathews80@gmail.com>
2018-04-23 11:52:15 -0500

    edited push to timeline function

--- js/pushToTimeline.js
+++ js/pushToTimeline.js
@@ -1,10 +1,11 @@
 //function to push interactions to the timeline array
-const pushToTimeline = (who, what, when) => {
+const pushToTimeline = (who, what) => {
+    const date = timeStamp()
     timeline.push(
         {
             who,
             what,
-            when
+            when: date
         }
     )
 }
